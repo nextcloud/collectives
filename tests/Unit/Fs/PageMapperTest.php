305488aa62f9a574feb90680f095a3f6da2451dc
Jonas Meurer <jonas@freesources.org>
2020-04-14 10:08:03 +0200

    PageMapper: Sanitise filenames and prevent filename conflicts
    * New function `sanitiseTitle()` to sanitise the title
    * New function `generateFilename()` to prevent filename conflicts:
      * Append ` (2)` if filename exists or replace ` (n)` with ` (n+1)`
        if filename exists and ends with ` (n)`.

---
+++ tests/Unit/Fs/PageMapperTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Unit\Fs;
+
+use OC\Files\Node\Folder;
+use OCA\Wiki\Fs\PageMapper;
+use OCP\Files\IRootFolder;
+use OCP\IDBConnection;
+use OCP\ILogger;
+use PHPUnit\Framework\TestCase;
+
+class PageMapperTest extends TestCase {
+	private $db;
+	private $root;
+	private $logger;
+	private $appName = 'wiki';
+	private $mapper;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->db = $this->getMockBuilder(IDBConnection::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$this->root = $this->getMockBuilder(IRootFolder::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$this->logger = $this->getMockBuilder(ILogger::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->mapper = new PageMapper($this->db, $this->root, $this->logger, $this->appName);
+	}
+
+	public function titleProvider(): array {
+		return [
+			['string with forbidden chars *|/\\:"<>?', 'string with forbidden chars'],
+			["string with forbidden UTF-8 chars #1: \xF0\x90\x80\x80", 'string with forbidden UTF-8 chars #1'],
+			["string with forbidden UTF-8 chars #2: \xF0\xBF\xBF\xBF", 'string with forbidden UTF-8 chars #2'],
+			["string with forbidden UTF-8 chars #3: \xF1\x80\x80\x80", 'string with forbidden UTF-8 chars #3'],
+			["string with forbidden UTF-8 chars #3: \xF4\x80\x80\x80", 'string with forbidden UTF-8 chars #3'],
+			["string with allowed UTF-8 chars #1: \x61\xC3\xB6\x61", "string with allowed UTF-8 chars #1 \x61\xC3\xB6\x61"],
+			['string with spaces', 'string with spaces'],
+			[' string with leading space', 'string with leading space'],
+			['.string with leading dot', 'string with leading dot'],
+			['string with trailing space ', 'string with trailing space'],
+			['', 'New Page']
+		];
+	}
+
+	/**
+	 * @dataProvider titleProvider
+	 *
+	 * @param string $input
+	 * @param string $output
+	 */
+	public function testSanitiseTitle(string $input, string $output): void {
+		$this->assertEquals($output, $this->mapper->sanitiseTitle($input));
+		$this->mapper->sanitiseTitle('abc');
+
+	}
+
+	public function filenameProvider(): array {
+		return [
+			['File exists1', 'File exists1 (2).md'],
+			['File exists2', 'File exists2 (4).md'],
+			['File exists2 (3)', 'File exists2 (4).md'],
+			['File exists3', 'File exists3 (2).md'],
+			['File exists4 (9)', 'File exists4 (10).md'],
+			['File exists5 (1i)', 'File exists5 (1i) (2).md'],
+			['File new', 'File new.md'],
+			[' (2)', ' (3).md']
+		];
+
+	}
+
+	/**
+	 * @dataProvider filenameProvider
+	 *
+	 * @param string $input
+	 * @param string $output
+	 */
+	public function testGenerateFilename(string $input, string $output): void {
+		$folder = $this->getMockBuilder(Folder::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$folder->method('nodeExists')
+			->willReturnMap([
+					['File exists1.md', true],
+					['File exists2.md', true],
+					['File exists2 (2).md', true],
+					['File exists2 (3).md', true],
+					['File exists3.md', true],
+					['File exists3 (1).md', true],
+					['File exists4 (9).md', true],
+					['File exists5 (1).md', true],
+					[' (2).md', true],
+					['File exists5 (1i).md', true]
+			]);
+
+		$this->assertEquals($output, PageMapper::generateFilename($folder, $input));
+    }
+}
