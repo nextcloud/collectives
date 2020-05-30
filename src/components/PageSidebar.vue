5876e048a205450e410e125dd8cf4b98fdee0bf5
Azul <azul@riseup.net>
2020-05-30 16:23:45 +0200

    js: use components for Page, PageSidebar and Start

---
+++ src/components/PageSidebar.vue
@@ -0,0 +1,31 @@
+<template>
+	<AppSidebar
+		ref="sidebar"
+		:title="'Page: ' + page.title"
+		subtitle="..."
+		@close="$emit('close')">
+		<SidebarVersionsTab :page-id="page.id" :page-title="page.title" />
+	</AppSidebar>
+</template>
+
+<script>
+import AppSidebar from '@nextcloud/vue/dist/Components/AppSidebar'
+import SidebarVersionsTab from './SidebarVersionsTab'
+
+export default {
+	name: 'PageSidebar',
+
+	components: {
+		AppSidebar,
+		SidebarVersionsTab,
+	},
+
+	props: {
+		page: {
+			type: Object,
+			required: true,
+		},
+	},
+}
+
+</script>
---
+++ src/components/Start.vue
@@ -0,0 +1,18 @@
+<template>
+	<AppContent>
+		<div id="emptycontent">
+			<div class="icon-file" />
+			<h2>{{ t('wiki', 'Create a page to get started') }}</h2>
+		</div>
+	</AppContent>
+</template>
+
+<script>
+import AppContent from '@nextcloud/vue/dist/Components/AppContent'
+export default {
+	name: 'Start',
+	components: {
+		AppContent,
+	},
+}
+</script>
