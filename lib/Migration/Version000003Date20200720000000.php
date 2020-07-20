75c3249b3fb312948647881ea716f4e80023d002
Jonas Meurer <jonas@freesources.org>
2020-07-20 16:41:32 +0200

    Add a Migration step to create wiki backend database
    * Also bump app version to 0.0.3 to trigger the migration step on
      upgrade

---
+++ lib/Migration/Version000003Date20200720000000.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OCA\Wiki\Migration;
+
+use Closure;
+use Doctrine\DBAL\Types\Type;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+class Version000003Date20200720000000 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('wiki')) {
+			$table = $schema->createTable('wiki');
+			$table->addColumn('id', Type::BIGINT, [
+				'autoincrement' => true,
+				'notnull' => true,
+			]);
+			$table->addColumn('circle_unique_id', Type::STRING, [
+				'notnull' => true,
+				'length' => 15,
+			]);
+			$table->addColumn('file_id', Type::BIGINT, [
+				'notnull' => true,
+			]);
+			$table->addColumn('owner_id', Type::STRING, [
+				'notnull' => true,
+				'length' => 64,
+			]);
+
+			$table->setPrimaryKey(['id']);
+			$table->addIndex(['circle_unique_id'], 'wiki_circle_unique_id_index');
+		}
+		return $schema;
+	}
+}
