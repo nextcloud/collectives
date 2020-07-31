528641902e163ea18038cff26fb2108ecc83854c
Jonas Meurer <jonas@freesources.org>
2020-07-31 12:21:02 +0200

    Refactor routes: Use `/_wikis/{wikiId}/_pages` as base URL for pages API

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -8,23 +8,23 @@
 
 return [
 	'routes' => [
-		// pages
-		['name' => 'page#index', 'url' => '/_pages/{wikiId}', 'verb' => 'GET',
-			'requirements' => ['wikiId' => '\d+']],
-		['name' => 'page#get', 'url' => '/_pages/{wikiId}/{id}', 'verb' => 'GET',
-			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
-		['name' => 'page#create', 'url' => '/_pages/{wikiId}', 'verb' => 'POST',
-			'requirements' => ['wikiId' => '\d+']],
-		['name' => 'page#rename', 'url' => '/_pages/{wikiId}/{id}', 'verb' => 'PUT',
-			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
-		['name' => 'page#destroy', 'url' => '/_pages/{wikiId}/{id}', 'verb' => 'DELETE',
-			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
-
 		// wikis
 		['name' => 'wiki#index', 'url' => '/_wikis', 'verb' => 'GET'],
 		['name' => 'wiki#create', 'url' => '/_wikis', 'verb' => 'POST'],
 		['name' => 'wiki#destroy', 'url' => '/_wikis/{id}', 'verb' => 'DELETE',
 			'requirements' => ['id' => '\d+']],
+
+		// pages
+		['name' => 'page#index', 'url' => '/_wikis/{wikiId}/_pages', 'verb' => 'GET',
+			'requirements' => ['wikiId' => '\d+']],
+		['name' => 'page#get', 'url' => '/_wikis/{wikiId}/_pages/{id}', 'verb' => 'GET',
+			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
+		['name' => 'page#create', 'url' => '/_wikis/{wikiId}/_pages', 'verb' => 'POST',
+			'requirements' => ['wikiId' => '\d+']],
+		['name' => 'page#rename', 'url' => '/_wikis/{wikiId}/_pages/{id}', 'verb' => 'PUT',
+			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
+		['name' => 'page#destroy', 'url' => '/_wikis/{wikiId}/_pages/{id}', 'verb' => 'DELETE',
+			'requirements' => ['wikiId' => '\d+', 'id' => '\d+']],
 
 		// default route
 		['name' => 'start#index', 'url' => '/{path}', 'verb' => 'GET',
