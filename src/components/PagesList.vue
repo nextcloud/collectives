a4cbf9897cfb168f678eabd0132352d3805537ea
Azul <azul@riseup.net>
2020-08-03 17:57:59 +0200

    ui: turn page avatars into solid colored pages
    Also use colors based on id
     - so they do not change because of a rename.

--- src/components/PagesList.vue
+++ src/components/PagesList.vue
@@ -5,7 +5,7 @@
 			:to="`/${$route.params.selectedWiki}/${page.title}`"
 			:class="{active: isActive(page)}"
 			class="app-content-list-item">
-			<div class="app-content-list-item-icon" :style="iconStyle(page.title)">
+			<div class="app-content-list-item-icon" :style="iconStyle(page.id)">
 				{{ page.title[0] }}
 			</div>
 			<div class="app-content-list-item-line-one">
@@ -53,7 +53,7 @@ export default {
 
 		iconStyle(id) {
 			const c = `page-${id}`.toRgb()
-			return `box-shadow: 3px 3px 7px rgb(${c.r}, ${c.g}, ${c.b})`
+			return `background-color: rgb(${c.r}, ${c.g}, ${c.b})`
 		},
 	},
 }
@@ -61,7 +61,9 @@ export default {
 
 <style lang="scss" scoped>
 	.app-content-list .app-content-list-item .app-content-list-item-icon {
-		border-radius: 0px;
-		color: grey;
+		border-radius: 3px 12px 3px 3px;
+		line-height: 40px;
+		width: 30px;
+		left: 12px;
 	}
 </style>
