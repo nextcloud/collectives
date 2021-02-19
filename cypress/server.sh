ed191e5e68909b17229aaf3d5e192386170689aa
Azul <azul@riseup.net>
2021-02-19 20:56:36 +0100

    cypress: prepare server with proper circles settings

--- cypress/server.sh
+++ cypress/server.sh
@@ -15,4 +15,8 @@ OC_PASS=bob php /var/www/html/occ user:add --password-from-env \
 OC_PASS=jane php /var/www/html/occ user:add --password-from-env \
 	--group='Bobs Group' jane
 php /var/www/html/occ config:app:set circles --value 1 allow_linked_groups
+php /var/www/html/occ config:system:set --type bool --value true -- allow_local_remote_servers
+php /var/www/html/occ config:app:set --value 1 -- circles self_signed_cert
+php /var/www/html/occ config:app:set --value 1 -- circles allow_non_ssl_links
+php /var/www/html/occ config:app:set --value 1 -- circles local_is_non_ssl
 "
