62be0eef2c5c79e27dc30411ac642473c0899b5b
Jonas Meurer <jonas@freesources.org>
2021-03-24 15:26:38 +0100

    Update local cypress environment to use our own Docker image

--- cypress/runLocal.sh
+++ cypress/runLocal.sh
@@ -13,8 +13,5 @@ docker-compose up -d --no-recreate
 npm install --no-save wait-on
 echo "starting to wait for server $CYPRESS_baseUrl"
 $(npm bin)/wait-on -i 500 -t 300000 $CYPRESS_baseUrl || (docker-compose logs && exit 1)
-docker-compose exec -T nextcloud bash /var/www/html/apps/collectives/cypress/server.sh
 
 (cd .. && $(npm bin)/cypress $@)
-
-
