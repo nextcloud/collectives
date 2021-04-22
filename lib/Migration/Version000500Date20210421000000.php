e24228df576bc792370da7ddc4e3c524c726deb7
Jonas Meurer <jonas@freesources.org>
2021-04-22 16:51:55 +0200

    Remove the migration job to split circle names and emojis
    We decided that splitting the (optional) emojis in circle names of
    existing collectives his is too error-prone.
    
    Instead, people will have to set the emoji for their existing
    collectives again and optionally rename the circle.
    
    Since the app isn't used a lot yet, we decided this to be acceptable.

--- lib/Migration/Version000500Date20210421000000.php
+++ lib/Migration/Version000500Date20210421000000.php
@@ -6,31 +6,11 @@ namespace OCA\Collectives\Migration;
 
 use Closure;
 use Doctrine\DBAL\Types\Types;
-use OCA\Circles\Db\CirclesRequest;
-use OCA\Circles\Exceptions\CircleAlreadyExistsException;
-use OCA\Circles\Exceptions\CircleDoesNotExistException;
-use OCA\Circles\Exceptions\ConfigNoCircleAvailableException;
-use OCA\Collectives\Db\CollectiveMapper;
 use OCP\DB\ISchemaWrapper;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
 
 class Version000500Date20210421000000 extends SimpleMigrationStep {
-	/** @var bool */
-	private $emojiMigration = false;
-
-	/** @var CollectiveMapper */
-	private $collectiveMapper;
-
-	/** @var CirclesRequest */
-	private $circlesRequest;
-
-	public function __construct(CollectiveMapper $collectiveMapper,
-								CirclesRequest $circlesRequest) {
-		$this->collectiveMapper = $collectiveMapper;
-		$this->circlesRequest = $circlesRequest;
-	}
-
 	/**
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
@@ -48,37 +28,7 @@ class Version000500Date20210421000000 extends SimpleMigrationStep {
 				'notnull' => false,
 				'length' => 8,
 			]);
-
-			$this->emojiMigration = true;
 		}
 		return $schema;
-	}
-
-	/**
-	 * @param IOutput $output
-	 * @param Closure $schemaClosure
-	 * @param array   $options
-	 */
-	public function postSchemaChange(IOutput $output, \Closure $schemaClosure, array $options): void {
-		if (!$this->emojiMigration) {
-			return;
-		}
-
-		// Split emojis and names in existing collectives
-		foreach ($this->collectiveMapper->getAll() as $collective) {
-			$oldName = $this->collectiveMapper->circleUniqueIdToName($collective->getCircleUniqueId());
-			[$name, $emoji] = EmojiSplitter::split($oldName);
-			if ($emoji) {
-				try {
-					$circle = $this->circlesRequest->getCircleFromUniqueId($collective->getCircleUniqueId());
-					$circle->setName($name);
-					$this->circlesRequest->updateCircle($circle);
-				} catch (CircleAlreadyExistsException | CircleDoesNotExistException | ConfigNoCircleAvailableException $e) {
-					return;
-				}
-				$collective->setEmoji($emoji);
-				$this->collectiveMapper->update($collective);
-			}
-		}
 	}
 }
