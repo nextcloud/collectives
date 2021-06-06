797ef9c352ae285421bac10cf2f2da08d4712976
Azul <azul@riseup.net>
2021-06-06 12:27:05 +0200

    feature: collective not found message
    Display a "Collective not found: name" message
    if no collective with the current `collectiveParam` could be found.
    
    See #154.

---
+++ src/components/CollectiveNotFound.vue
@@ -0,0 +1,33 @@
+<template>
+	<EmptyContent icon="icon-collectives">
+		{{ t('collectives', 'Collective not found:') }}
+		{{ collectiveParam }}
+		<template #desc>
+			<div>
+				{{ t('collectives', 'You\'re not part of a collective with that name.') }}
+			</div>
+			<div>
+				{{ t('collectives', 'Select a collective or create a new one on the left.') }}
+			</div>
+		</template>
+	</EmptyContent>
+</template>
+
+<script>
+import EmptyContent from '@nextcloud/vue/dist/Components/EmptyContent'
+import { mapGetters } from 'vuex'
+
+export default {
+	name: 'CollectiveNotFound',
+
+	components: {
+		EmptyContent,
+	},
+
+	computed: {
+		...mapGetters([
+			'collectiveParam',
+		]),
+	},
+}
+</script>
