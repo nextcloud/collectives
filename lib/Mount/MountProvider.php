1bc152172375f2809e1d43297e7434ae933b63b4
Jonas Meurer <jonas@freesources.org>
2021-06-09 12:33:43 +0200

    Better exception handling in UserFolderHelper and MountProvider

--- lib/Mount/MountProvider.php
+++ lib/Mount/MountProvider.php
@@ -6,12 +6,15 @@ use OC\Files\Cache\Cache;
 use OCA\Collectives\Fs\NodeHelper;
 use OCA\Collectives\Fs\UserFolderHelper;
 use OCA\Collectives\Service\CollectiveHelper;
+use OCA\Collectives\Service\NotFoundException;
+use OCA\Collectives\Service\NotPermittedException;
 use OCP\App\IAppManager;
 use OCP\AppFramework\QueryException;
+use OCP\DB\Exception;
 use OCP\Files\Config\IMountProvider;
 use OCP\Files\IMimeTypeLoader;
 use OCP\Files\Mount\IMountPoint;
-use OCP\Files\NotFoundException;
+use OCP\Files\NotFoundException as FilesNotFoundException;
 use OCP\Files\Storage\IStorageFactory;
 use OCP\IUser;
 
@@ -64,17 +67,25 @@ class MountProvider implements IMountProvider {
 	 *
 	 * @return array
 	 * @throws NotFoundException
-	 * @throws QueryException
+	 * @throws NotPermittedException
 	 */
 	public function getFoldersForUser(IUser $user): array {
 		$folders = [];
 		if (!$this->appManager->isEnabledForUser('circles', $user)) {
 			return $folders;
 		}
-		$collectiveInfos = $this->collectiveHelper->getCollectivesForUser($user->getUID(), false);
+		try {
+			$collectiveInfos = $this->collectiveHelper->getCollectivesForUser($user->getUID(), false);
+		} catch (QueryException $e) {
+			throw new NotFoundException($e->getMessage());
+		}
 		foreach ($collectiveInfos as $c) {
 			$mountPointName = $this->nodeHelper->sanitiseFilename($c->getName());
-			$cacheEntry = $this->collectiveFolderManager->getFolderFileCache($c->getId(), $mountPointName);
+			try {
+				$cacheEntry = $this->collectiveFolderManager->getFolderFileCache($c->getId(), $mountPointName);
+			} catch (FilesNotFoundException | Exception $e) {
+				throw new NotFoundException($e->getMessage());
+			}
 			$folders[] = [
 				'folder_id' => $c->getId(),
 				'mount_point' => $this->userFolderHelper->get($user->getUID())->getName() . '/' . $mountPointName,
@@ -90,18 +101,23 @@ class MountProvider implements IMountProvider {
 	 *
 	 * @return IMountPoint[]
 	 * @throws NotFoundException
+	 * @throws NotPermittedException
 	 */
 	public function getMountsForUser(IUser $user, IStorageFactory $loader) {
 		$folders = $this->getFoldersForUser($user);
 
-		return array_map(function ($folder) use ($user, $loader) {
-			return $this->collectiveFolderManager->getMount(
-				$folder['folder_id'],
-				'/' . $user->getUID() . '/files/' . $folder['mount_point'],
-				$folder['rootCacheEntry'],
-				$loader,
-				$user
-			);
-		}, $folders);
+		try {
+			return array_map(function ($folder) use ($user, $loader) {
+				return $this->collectiveFolderManager->getMount(
+					$folder['folder_id'],
+					'/' . $user->getUID() . '/files/' . $folder['mount_point'],
+					$folder['rootCacheEntry'],
+					$loader,
+					$user
+				);
+			}, $folders);
+		} catch (FilesNotFoundException | \Exception $e) {
+			throw new NotFoundException($e->getMessage());
+		}
 	}
 }
