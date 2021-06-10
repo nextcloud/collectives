211929345adb6e4b334a7d498e8cec0d9a0593f8
Jonas Meurer <jonas@freesources.org>
2021-06-10 17:28:44 +0200

    Migrate to Circles22
    * Migrate backend CircleHelper to circles22 interface
    * Add DB migration to change length for `circle_unique_id` to 31
    * Use new OCS route to access circles API from frontend and integration
      tests

--- lib/Service/CircleHelper.php
+++ lib/Service/CircleHelper.php
@@ -3,14 +3,50 @@
 
 namespace OCA\Collectives\Service;
 
-use OCA\Circles\Api\v1\Circles;
-use OCA\Circles\Exceptions\CircleAlreadyExistsException;
-use OCA\Circles\Exceptions\CircleDoesNotExistException;
-use OCA\Circles\Exceptions\MemberDoesNotExistException;
+use daita\MySmallPhpTools\Exceptions\InvalidItemException;
+use OCA\Circles\CirclesManager;
+use OCA\Circles\Exceptions\CircleNotFoundException;
+use OCA\Circles\Exceptions\FederatedItemException;
+use OCA\Circles\Exceptions\FederatedUserException;
+use OCA\Circles\Exceptions\FederatedUserNotFoundException;
+use OCA\Circles\Exceptions\InitiatorNotFoundException;
+use OCA\Circles\Exceptions\InvalidIdException;
+use OCA\Circles\Exceptions\RequestBuilderException;
+use OCA\Circles\Exceptions\SingleCircleNotFoundException;
 use OCA\Circles\Model\Circle;
-use OCP\AppFramework\QueryException;
+use OCA\Circles\Model\FederatedUser;
+use OCA\Circles\Model\Member;
 
 class CircleHelper {
+	/** @var CirclesManager */
+	private $circlesManager;
+
+	public function __construct(CirclesManager $circlesManager) {
+		$this->circlesManager = $circlesManager;
+	}
+
+	/**
+	 * @param string|null $userId
+	 *
+	 * @return FederatedUser|null
+	 * @throws NotFoundException
+	 * @throws NotPermittedException
+	 */
+	private function getFederatedUser(?string $userId = null): ?FederatedUser {
+		if (null === $userId) {
+			return null;
+		}
+
+		try {
+			return $this->circlesManager->getFederatedUser($userId, Member::TYPE_USER);
+		} catch (CircleNotFoundException $e) {
+			throw new NotFoundException($e->getMessage());
+		} catch (FederatedItemException |
+				 RequestBuilderException $e) {
+			throw new NotPermittedException($e->getMessage());
+		}
+	}
+
 	/**
 	 * @param string|null $userId
 	 *
@@ -19,11 +55,21 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function getCircles(?string $userId = null): array {
+		$federatedUser = $this->getFederatedUser($userId);
 		try {
-			return Circles::joinedCircles($userId);
-		} catch (QueryException $e) {
-			throw new NotFoundException($e->getMessage());
+			$this->circlesManager->startSession($federatedUser);
+			$circles = $this->circlesManager->getCircles();
+		} catch (FederatedUserNotFoundException |
+				 SingleCircleNotFoundException |
+				 RequestBuilderException |
+				 InvalidIdException |
+				 FederatedUserException |
+				 InitiatorNotFoundException $e) {
+			throw new NotPermittedException($e->getMessage());
 		}
+		$this->circlesManager->stopSession();
+
+		return $circles;
 	}
 
 	/**
@@ -36,11 +82,27 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function getCircle(string $circleId, ?string $userId = null, bool $super = false): Circle {
+		$federatedUser = $this->getFederatedUser($userId);
 		try {
-			return Circles::detailsCircle($circleId, true);
-		} catch (CircleDoesNotExistException $e) {
+			if ($super) {
+				$this->circlesManager->startSuperSession();
+			} else {
+				$this->circlesManager->startSession($federatedUser);
+			}
+			$circle = $this->circlesManager->getCircle($circleId);
+		} catch (CircleNotFoundException $e) {
 			throw new NotFoundException($e->getMessage());
+		} catch (FederatedUserNotFoundException |
+				 SingleCircleNotFoundException |
+				 RequestBuilderException |
+				 InvalidIdException |
+				 FederatedUserException |
+				 InitiatorNotFoundException $e) {
+			throw new NotPermittedException($e->getMessage());
 		}
+		$this->circlesManager->stopSession();
+
+		return $circle;
 	}
 
 	/**
@@ -53,17 +115,42 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function findCircle(string $name, string $userId, bool $admin = true): ?Circle {
-		$circles = Circles::listCircles(
-			Circles::CIRCLES_ALL & ~Circles::CIRCLES_PERSONAL,
-			$name,
-			Circles::LEVEL_ADMIN
-		);
+		$circles = $this->getCircles($userId);
 		foreach ($circles as $circle) {
-			if (strtolower($circle->getName()) === strtolower($name)) {
+			if (!strcmp(strtolower($circle->getName()), strtolower($name)) ||
+				!strcmp(strtolower($circle->getSanitizedName()), strtolower($name))) {
+				if ($admin && !$this->isAdmin($circle->getSingleId(), $userId)) {
+					return null;
+				}
 				return $circle;
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return bool
+	 * @throws NotPermittedException
+	 */
+	private function existsCircle(string $name): bool {
+		$this->circlesManager->startSuperSession();
+		try {
+			$circles = $this->circlesManager->getCircles();
+		} catch (InitiatorNotFoundException | RequestBuilderException $e) {
+			throw new NotPermittedException($e->getMessage());
+		}
+		$this->circlesManager->stopSession();
+
+		foreach ($circles as $circle) {
+			if (!strcmp(strtolower($circle->getName()), strtolower($name)) ||
+				!strcmp(strtolower($circle->getSanitizedName()), strtolower($name))) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	/**
@@ -76,11 +163,26 @@ class CircleHelper {
 	 * @throws CircleExistsException
 	 */
 	public function createCircle(string $name, ?string $userId = null): Circle {
+		$federatedUser = $this->getFederatedUser($userId);
 		try {
-			$circle = Circles::createCircle(Circles::CIRCLES_SECRET, $name);
-		} catch (CircleAlreadyExistsException $e) {
-			throw new CircleExistsException($e->getMessage());
+			if ($this->existsCircle($name)) {
+				throw new CircleExistsException('A circle with that name exists');
+			}
+			$this->circlesManager->startSession($federatedUser);
+			$circle = $this->circlesManager->createCircle($name, null, false, false);
+		} catch (CircleNotFoundException $e) {
+			throw new NotFoundException($e->getMessage());
+		} catch (FederatedUserNotFoundException |
+				 SingleCircleNotFoundException |
+				 RequestBuilderException |
+				 InvalidIdException |
+				 FederatedUserException |
+				 InitiatorNotFoundException |
+				 FederatedItemException |
+				 InvalidItemException $e) {
+			throw new NotPermittedException($e->getMessage());
 		}
+		$this->circlesManager->stopSession();
 
 		return $circle;
 	}
@@ -93,11 +195,22 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function destroyCircle(string $circleId, ?string $userId = null): void {
+		$federatedUser = $this->getFederatedUser($userId);
 		try {
-			Circles::destroyCircle($circleId);
-		} catch (CircleDoesNotExistException $e) {
+			$this->circlesManager->startSession($federatedUser);
+			$this->circlesManager->destroyCircle($circleId);
+		} catch (CircleNotFoundException $e) {
 			throw new NotFoundException($e->getMessage());
+		} catch (FederatedUserNotFoundException |
+				 SingleCircleNotFoundException |
+				 RequestBuilderException |
+				 InvalidIdException |
+				 FederatedUserException |
+				 InitiatorNotFoundException |
+				 FederatedItemException $e) {
+			throw new NotPermittedException($e->getMessage());
 		}
+		$this->circlesManager->stopSession();
 	}
 
 	/**
@@ -110,14 +223,16 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function isMember(string $circleId, string $userId, bool $admin = false): bool {
-		try {
-			$joinedCircles = Circles::joinedCircles($userId);
-			foreach ($joinedCircles as $jc) {
-				if ($circleId === $jc->getUniqueId()) {
-					return true;
-				}
+		/** @var FederatedUser $federatedUser */
+		$federatedUser = $this->getFederatedUser($userId);
+
+		$level = $admin ? Member::LEVEL_OWNER : Member::LEVEL_MEMBER;
+		$memberships = $federatedUser->getMemberships();
+		foreach ($memberships as $membership) {
+			if ($membership->getCircleId() === $circleId &&
+				$membership->getLevel() >= $level) {
+				return true;
 			}
-		} catch (QueryException $e) {
 		}
 		return false;
 	}
@@ -131,15 +246,6 @@ class CircleHelper {
 	 * @throws NotPermittedException
 	 */
 	public function isAdmin(string $circleId, string $userId): bool {
-		try {
-			$member = Circles::getMember(
-				$circleId,
-				$userId,
-				Circles::TYPE_USER);
-			// For now only circle owners are admins for the collective
-			return ($member !== null && $member->getLevel() >= Circles::LEVEL_OWNER);
-		} catch (MemberDoesNotExistException $e) {
-			return false;
-		}
+		return $this->isMember($circleId, $userId, true);
 	}
 }
--- templates/main.php
+++ templates/main.php
@@ -1,8 +1,4 @@
 <?php
-script('circles', 'circles.v1.circles');
-script('circles', 'circles.v1.members');
-script('circles', 'circles.v1.links');
-script('circles', 'circles.v1');
 script('collectives', 'collectives-main');
 style('collectives', 'icons');
 ?>
