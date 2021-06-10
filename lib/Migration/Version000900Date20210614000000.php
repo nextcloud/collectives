f38aff056327d98c24e3ea98b447f21e7c50df09
Jonas Meurer <jonas@freesources.org>
2021-06-10 17:28:44 +0200

    Migrate to Circles22
    * Migrate backend CircleHelper to circles22 interface
    * Add DB migration to change length for `circle_unique_id` to 31
    * Use new OCS route to access circles API from frontend and integration
      tests

---
+++ lib/Migration/Version000900Date20210614000000.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Collectives\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+class Version000900Date20210614000000 extends SimpleMigrationStep {
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array   $options
+	 *
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('collectives');
+		if ($table->hasColumn('circle_unique_id')) {
+			$table->changeColumn('circle_unique_id', [
+				'length' => 31,
+			]);
+		}
+		return $schema;
+	}
+}
--- src/store/circles.js
+++ src/store/circles.js
@@ -1,3 +1,5 @@
+import axios from '@nextcloud/axios'
+import { generateOcsUrl } from '@nextcloud/router'
 import { GET_CIRCLES } from './actions'
 import { DELETE_CIRCLE_FOR, SET_CIRCLES } from './mutations'
 
@@ -10,7 +12,7 @@ export default {
 		availableCircles(state, _getters, rootState) {
 			return state.circles.filter(circle => {
 				const matchCircleId = c => {
-					return (c.circleId === circle.unique_id)
+					return (c.circleId === circle.id)
 				}
 				const alive = rootState.collectives.collectives.find(matchCircleId)
 				const trashed = rootState.collectives.trashCollectives.find(matchCircleId)
@@ -24,7 +26,7 @@ export default {
 			state.circles = circles
 		},
 		[DELETE_CIRCLE_FOR](state, collective) {
-			state.circles.splice(state.circles.findIndex(c => c.unique_id === collective.circleId), 1)
+			state.circles.splice(state.circles.findIndex(c => c.id === collective.circleId), 1)
 		},
 	},
 
@@ -33,10 +35,8 @@ export default {
 		 * Get list of all circles
 		 */
 		async [GET_CIRCLES]({ commit }) {
-			const api = OCA.Circles.api
-			api.listCircles('all', '', 9, response => {
-				commit(SET_CIRCLES, response.data)
-			})
+			const response = await axios.get(generateOcsUrl('apps/circles/circles'))
+			commit(SET_CIRCLES, response.data.ocs.data)
 		},
 	},
 }
