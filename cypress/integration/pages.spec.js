acdf46692e147878e01a813ca9af8f099d629525
Raul <r.ferreira.fuentes@gmail.com>
2022-08-04 17:42:43 +0200

    Reenable cypress search test
    Signed-off-by: Raul <r.ferreira.fuentes@gmail.com>
    Signed-off-by: raul <raul@nextcloud.com>

--- cypress/integration/pages.spec.js
+++ cypress/integration/pages.spec.js
@@ -226,16 +226,21 @@ describe('Page', function() {
 		})
 	})
 
-	describe('Using the search providers', function() {
+	describe('Using the search providers to search for a page', function() {
 		it('Search for page and page content', function() {
 			cy.get('.unified-search a').click()
 			cy.get('.unified-search__form input')
-				.type('Day 2')
-			cy.get('.unified-search__results-collectives-pages').should('contain', 'Day 2')
-			/*
-			 * Removed because the pages do not get indexed during the tests, so the check fails:
-			cy.get('.unified-search__results-collectives-page-content').should('contain', 'with Day 2 in')
-			 */
+				.type('Search')
+			cy.get('.unified-search__results-collectives-pages').should('contain', 'SearchTest')
+		})
+	})
+
+	describe('Using the search providers to search page content', function() {
+		it('Search for page and page content', function() {
+			cy.get('.unified-search a').click()
+			cy.get('.unified-search__form input')
+				.type('share your thoughts')
+			cy.get('.unified-search__results-collectives-page-content').should('contain', 'your thoughts that really matter. Whether it')
 		})
 	})
 
