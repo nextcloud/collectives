e4ca2a1325a2392effe1a08427696e977ed97300
Max <max@nextcloud.com>
2023-10-17 13:50:04 +0200

    test(cy): click on page instead of visit
    Navigate inside the collective instead of loading the entire page again.
    
    Original test run: 57 sec.
    After this commit: 42 sec.
    
    Signed-off-by: Max <max@nextcloud.com>

--- cypress/e2e/page-details.spec.js
+++ cypress/e2e/page-details.spec.js
@@ -42,7 +42,7 @@ describe('Page details', function() {
 
 	describe('Display table of contents', function() {
 		it('Allows to display/close TOC and switch page modes in between', function() {
-			cy.visit('/apps/collectives/Our%20Garden/TableOfContents')
+			cy.contains('.app-content-list-item a', 'TableOfContents').click()
 			cy.get('#titleform .action-item__menutoggle')
 				.click()
 
@@ -73,7 +73,7 @@ describe('Page details', function() {
 	describe('Displaying backlinks', function() {
 		it('Lists backlinks for a page', function() {
 			cy.intercept('PUT', '**/apps/text/session/create').as('textCreateSession')
-			cy.visit('/apps/collectives/Our%20Garden/Day%201')
+			cy.contains('.app-content-list-item a', 'Day 1').click()
 			cy.wait('@textCreateSession')
 			cy.get('button.action-item .icon-menu-sidebar').click()
 			cy.get('a#backlinks').click()
