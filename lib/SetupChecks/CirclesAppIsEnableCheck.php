d6e85e02a4903033d76a7a1241087ad29478b7b5
Hoang Pham <hoangmaths96@gmail.com>
2024-05-14 13:48:55 +0700

    feat(setup-checks): sqlite checks
    Signed-off-by: Hoang Pham <hoangmaths96@gmail.com>

---
+++ lib/SetupChecks/CirclesAppIsEnableCheck.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Collectives\SetupChecks;
+
+use OCP\App\IAppManager;
+use OCP\IL10N;
+use OCP\SetupCheck\ISetupCheck;
+use OCP\SetupCheck\SetupResult;
+
+class CirclesAppIsEnableCheck implements ISetupCheck {
+	public function __construct(
+		private IL10N       $l10n,
+		private IAppManager $appManager
+	) {
+	}
+
+
+	public function getCategory(): string {
+		return 'app';
+	}
+
+	public function getName(): string {
+		return $this->l10n->t('Teams App Enabled');
+	}
+
+	public function run(): SetupResult {
+		if ($this->appManager->isEnabledForUser('circles')) {
+			return SetupResult::success($this->l10n->t('Teams app is enabled'));
+		}
+
+		return SetupResult::error($this->l10n->t('The teams app is not enabled, but is required for Collectives to work.'));
+	}
+}
---
+++ lib/SetupChecks/PDOSQLiteDriverIsEnableCheck.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Collectives\SetupChecks;
+
+use OCA\Collectives\Service\SearchService;
+use OCP\IL10N;
+use OCP\SetupCheck\ISetupCheck;
+use OCP\SetupCheck\SetupResult;
+
+class PDOSQLiteDriverIsEnableCheck implements ISetupCheck {
+	public function __construct(
+		private IL10N         $l10n,
+		private SearchService $searchService
+	) {
+	}
+
+
+	public function getCategory(): string {
+		return 'database';
+	}
+
+	public function getName(): string {
+		return $this->l10n->t('PDO SQLite driver');
+	}
+
+	public function run(): SetupResult {
+		if ($this->searchService->areDependenciesMet()) {
+			return SetupResult::success($this->l10n->t('PDO SQLite driver is enabled, full text search of page content is available.'));
+		}
+
+		return SetupResult::error(
+			$this->l10n->t('Collectives app is enabled, but PDO SQLite driver is missing. Please install it to enable full text search of the page content.')
+		);
+	}
+}
