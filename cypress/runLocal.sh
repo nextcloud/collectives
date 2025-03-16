13a2c6be5aaf298e2655bdda523f10e64615cb94
Kostiantyn Miakshyn <molodchick@gmail.com>
2025-03-16 01:12:28 +0100

    Slugify Collectives and Pages (fix psalm)
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

--- cypress/runLocal.sh
+++ cypress/runLocal.sh
@@ -8,7 +8,7 @@ export APP_SOURCE="$PWD/.."
 export LANG="en_EN.UTF-8"
 
 function finish {
-	docker-compose down
+	docker compose down
 }
 trap finish EXIT
 
@@ -21,11 +21,11 @@ if npm exec wait-on -- -i 500 -t 2000 "$CYPRESS_baseUrl" 2>/dev/null; then
 	echo Server is up at "$CYPRESS_baseUrl"
 else
 	echo No server reached at "$CYPRESS_baseUrl" - starting containers.
-	DOCKER_BUILDKIT=1 docker-compose up -d
+	DOCKER_BUILDKIT=1 docker compose up -d
 	if ! npm exec wait-on -- -i 500 -t 240000 "$CYPRESS_baseUrl" 2>/dev/null; then
 		echo Waiting for "$CYPRESS_baseUrl" timed out.
 		echo Container logs:
-		docker-compose logs
+		docker compose logs
 		exit 1
 	fi
 fi
