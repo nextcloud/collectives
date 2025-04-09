f9cd131191b32948879b33cb4f5623c28663c297
Jonas <jonas@freesources.org>
2025-04-09 10:25:42 +0200

    refactor(Page): move page title into its own component
    Signed-off-by: Jonas <jonas@freesources.org>

---
+++ src/components/Page/PageTitle.vue
@@ -0,0 +1,348 @@
+<!--
+  - SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<div class="title-container">
+		<h2 id="titleform" class="page-title" :class="{ 'pre-nc30': isPreNc30 }">
+			<!-- Page emoji or icon -->
+			<div class="page-title-icon"
+				:class="{ 'mobile': isMobile }">
+				<!-- Landing page: collective emoji or CollectivesIcon -->
+				<div v-if="isLandingPage && currentCollective.emoji">
+					{{ currentCollective.emoji }}
+				</div>
+				<CollectivesIcon v-else-if="isLandingPage" :size="pageTitleIconSize" fill-color="var(--color-text-maxcontrast)" />
+				<PageTemplateIcon v-else-if="isTemplatePage" :size="pageTitleIconSize" fill-color="var(--color-text-maxcontrast)" />
+
+				<!-- Emoji picker if editable -->
+				<NcEmojiPicker v-else-if="currentCollectiveCanEdit"
+					ref="page-emoji-picker"
+					:show-preview="true"
+					:allow-unselect="true"
+					:selected-emoji="currentPage.emoji"
+					@select="onSelectEmoji"
+					@unselect="onUnselectEmoji">
+					<NcButton type="tertiary"
+						:aria-label="t('collectives', 'Select emoji for page')"
+						:title="t('collectives', 'Select emoji')"
+						class="button-emoji-page"
+						:class="{ 'mobile': isMobile }"
+						@click.prevent>
+						<template #icon>
+							<NcLoadingIcon v-if="emojiButtonIsLoading"
+								:size="pageTitleIconSize"
+								fill-color="var(--color-text-maxcontrast)" />
+							<div v-else-if="currentPage.emoji">
+								{{ currentPage.emoji }}
+							</div>
+							<EmoticonOutlineIcon v-else
+								class="emoji-picker-emoticon"
+								:size="pageTitleIconSize"
+								fill-color="var(--color-text-maxcontrast)" />
+						</template>
+					</NcButton>
+				</NcEmojiPicker>
+
+				<!-- Page emoji or PageIcon if not editable -->
+				<template v-else>
+					<div v-if="currentPage.emoji">
+						{{ currentPage.emoji }}
+					</div>
+					<EmoticonOutlineIcon v-else
+						class="emoji-picker-emoticon"
+						:size="pageTitleIconSize"
+						fill-color="var(--color-text-maxcontrast)" />
+				</template>
+			</div>
+
+			<!-- Page title -->
+			<form @submit.prevent="focusEditor()">
+				<input v-if="isLandingPage"
+					ref="landingPageTitle"
+					:title="titleIfTruncated(currentCollective.name)"
+					class="title"
+					:class="{ 'mobile': isMobile }"
+					type="text"
+					disabled
+					:value="currentCollective.name">
+				<input v-else-if="isTemplatePage"
+					class="title"
+					:class="{ 'mobile': isMobile }"
+					type="text"
+					disabled
+					:value="t('collectives', 'Template')">
+				<input v-else
+					ref="title"
+					v-model="newTitle"
+					:title="titleIfTruncated(newTitle)"
+					class="title"
+					:class="{ 'mobile': isMobile }"
+					:placeholder="t('collectives', 'Title')"
+					type="text"
+					:disabled="!currentCollectiveCanEdit"
+					@blur="onTitleBlur()"
+					@keydown.stop="onTitleKeyDown">
+			</form>
+
+			<div class="titlebar-buttons" :class="{'titlebar-buttons_sidebar-toggle': !isMobile && !showing('sidebar')}">
+				<!-- Edit button if editable -->
+				<EditButton v-if="currentCollectiveCanEdit"
+					:mobile="isMobile"
+					class="edit-button" />
+
+				<!-- Actions menu -->
+				<PageActionMenu :show-files-link="!isPublic"
+					:page-id="currentPage.id"
+					:parent-id="currentPage.parentId"
+					:timestamp="currentPage.timestamp"
+					:last-user-id="currentPage.lastUserId"
+					:last-user-display-name="currentPage.lastUserDisplayName"
+					:is-landing-page="isLandingPage"
+					:is-template="isTemplatePage" />
+			</div>
+		</h2>
+	</div>
+</template>
+<script>
+import isMobile from '@nextcloud/vue/dist/Mixins/isMobile.js'
+import pageMixin from '../../mixins/pageMixin.js'
+
+import { mapActions, mapState } from 'pinia'
+import { useRootStore } from '../../stores/root.js'
+import { useCollectivesStore } from '../../stores/collectives.js'
+import { usePagesStore } from '../../stores/pages.js'
+import { showError } from '@nextcloud/dialogs'
+
+import { NcButton, NcLoadingIcon } from '@nextcloud/vue'
+import NcEmojiPicker from '@nextcloud/vue/dist/Components/NcEmojiPicker.js'
+import CollectivesIcon from '../Icon/CollectivesIcon.vue'
+import EmoticonOutlineIcon from 'vue-material-design-icons/EmoticonOutline.vue'
+import EditButton from './EditButton.vue'
+import PageActionMenu from './PageActionMenu.vue'
+import PageTemplateIcon from '../Icon/PageTemplateIcon.vue'
+
+export default {
+	name: 'PageTitle',
+
+	components: {
+		CollectivesIcon,
+		EditButton,
+		EmoticonOutlineIcon,
+		NcButton,
+		NcEmojiPicker,
+		NcLoadingIcon,
+		PageActionMenu,
+		PageTemplateIcon,
+	},
+
+	mixins: [
+		isMobile,
+		pageMixin,
+	],
+
+	data() {
+		return {
+			newTitle: '',
+			titleIsTruncated: false,
+		}
+	},
+
+	computed: {
+		...mapState(useRootStore, [
+			'isPublic',
+			'isTextEdit',
+			'loading',
+			'showing',
+		]),
+		...mapState(useCollectivesStore, [
+			'currentCollective',
+			'currentCollectiveCanEdit',
+		]),
+		...mapState(usePagesStore, [
+			'currentPage',
+			'isIndexPage',
+			'isTemplatePage',
+			'isLandingPage',
+		]),
+
+		// TODO: remove when we stop supporting NC < 30
+		isPreNc30() {
+			return window.getComputedStyle(document.body).getPropertyValue('--default-clickable-area') === '44px'
+		},
+
+		hasSidebarToggle() {
+			return !this.showing('sidebar')
+		},
+
+		titleChanged() {
+			return this.newTitle && this.newTitle !== this.currentPage.title
+		},
+
+		documentTitle() {
+			const { filePath, title } = this.currentPage
+			const parts = [
+				this.currentCollective.name,
+				t('collectives', 'Collectives'),
+				'Nextcloud',
+			]
+			if (!this.isLandingPage) {
+				// Add parent page names in reverse order
+				filePath.split('/').forEach(part => part && parts.unshift(part))
+				if (!this.isIndexPage) {
+					parts.unshift(title)
+				}
+			}
+			return parts.join(' - ')
+		},
+
+		titleIfTruncated() {
+			return (title) => this.titleIsTruncated ? title : null
+		},
+
+		emojiButtonIsLoading() {
+			return this.loading(`pageEmoji-${this.currentPage.id}`)
+		},
+
+		showingPageEmojiPicker() {
+			return this.showing('pageEmojiPicker')
+		},
+
+		pageTitleIconSize() {
+			return isMobile ? 25 : 30
+		},
+	},
+
+	watch: {
+		'documentTitle'() {
+			document.title = this.documentTitle
+		},
+
+		'newTitle'() {
+			this.$nextTick(() => {
+				if (this.$refs.title) {
+					this.titleIsTruncated = this.$refs.title.scrollWidth > this.$refs.title.clientWidth
+
+				} else if (this.$refs.landingPageTitle) {
+					this.titleIsTruncated = this.$refs.landingPageTitle.scrollWidth > this.$refs.landingPageTitle.clientWidth
+				}
+			})
+		},
+
+		'showingPageEmojiPicker'(val) {
+			if (val === true) {
+				this.openPageEmojiPicker()
+			}
+		},
+
+		'currentPage.id'() {
+			this.initTitleEntry()
+			this.hide('outline')
+		},
+	},
+
+	mounted() {
+		document.title = this.documentTitle
+		this.initTitleEntry()
+	},
+
+	methods: {
+		...mapActions(useRootStore, [
+			'done',
+			'hide',
+			'load',
+		]),
+		...mapActions(usePagesStore, [
+			'getPages',
+			'renamePage',
+		]),
+
+		initTitleEntry() {
+			if (this.loading('newPageTitle')) {
+				this.newTitle = ''
+				this.$nextTick(this.focusTitle)
+				this.done('newPageTitle')
+				return
+			}
+			this.newTitle = this.currentPage.title
+		},
+
+		focusTitle() {
+			this.$refs.title.focus()
+		},
+
+		focusEditor() {
+			this.$emit('focus-editor')
+		},
+
+		saveEditor() {
+			this.$emit('save-editor')
+		},
+
+		async onSelectEmoji(emoji) {
+			await this.setEmoji(this.currentPage.id, emoji)
+		},
+
+		onUnselectEmoji() {
+			return this.onSelectEmoji('')
+		},
+
+		openPageEmojiPicker() {
+			this.$refs['page-emoji-picker'].open = true
+			this.hide('pageEmojiPicker')
+		},
+
+		/**
+		 * Rename currentPage on the server
+		 */
+		async onTitleBlur() {
+			if (!this.titleChanged) {
+				return
+			}
+			try {
+				await this.renamePage(this.newTitle)
+				// The resulting title may be different due to sanitizing
+				this.newTitle = this.currentPage.title
+				this.getPages(false)
+			} catch (e) {
+				console.error(e)
+				showError(t('collectives', 'Could not rename the page'))
+			}
+		},
+
+		onTitleKeyDown(event) {
+			if (this.isTextEdit && (event.ctrlKey || event.metaKey) && event.key === 's') {
+				this.saveEditor()
+				event.preventDefault()
+			}
+		},
+	},
+}
+</script>
+
+<style scoped lang="scss">
+// TODO: remove when we stop supporting NC < 30
+.page-title.pre-nc30 {
+	padding-top: 11px;
+}
+
+.titlebar-buttons {
+	display: flex;
+	gap: 4px;
+	align-items: center;
+
+	&_sidebar-toggle {
+		margin-right: calc(var(--default-clickable-area) + 2px);
+	}
+}
+</style>
+
+<style lang="scss">
+@media print {
+	/* Don't print emoticon button (if page doesn't have an emoji set) */
+	.edit-button, .action-item, .emoji-picker-emoticon {
+		display: none !important;
+	}
+}
+</style>
