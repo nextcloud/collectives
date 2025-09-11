507a7dd99878f0873f4791f224d049b548209b03
Jonas <jonas@freesources.org>
2025-09-11 16:37:29 +0200

    feat(serviceWorker): Bring back precache for collectives app assets
    Need to disable `navigateFeedback` explicitely as configuring precache
    seems to auto-enable it.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- vite.config.js
+++ vite.config.js
@@ -48,29 +48,34 @@ export default createAppConfig(
 						inlineWorkboxRuntime: true,
 						// Let all clients use the new service worker version immediately after update
 						skipWaiting: true,
-						// Disable precaching
-						globPatterns: undefined,
-						globDirectory: undefined,
-						globIgnores: undefined,
+						// Precache for collectives app assets
+						globPatterns: ["**/*.{js,css,mjs}"],
+						maximumFileSizeToCacheInBytes: 5242880,
+
+						// Disable navigateFallback (required as precache seems to auto-enable it)
+						navigateFallback: null,
+
 						// Don't create a sourcemap for the service worker files
 						// sourcemap: false,
 
 						// Configure cache responses for runtime routes
 						runtimeCaching: [
-							// Cache for navigation requests within the vue router scope
 							{
+								// Cache for navigation requests within the vue router scope
+
 								// Strategy: try network first, then cache
 								handler: 'NetworkFirst',
 								// Cache assets and API requests
 								// urlPattern: /^.*/,
+								// urlPattern: /(?:\/index\.php)?\/apps\/collectives/,
 								urlPattern: ({ url, request }) => request.mode === 'navigate'
 									&& url.pathname.match(/(?:\/index\.php)?\/apps\/collectives/),
+
 								options: {
 									cacheName: 'collectives-vue-router',
-									// Cache max 10000 requests for one week
+									// Cache max for one week
 									expiration: {
 										maxAgeSeconds: 3600 * 24 * 7, // one week
-										maxEntries: 10000,
 									},
 									plugins: [{
 										cacheKeyWillBeUsed: async () => {
@@ -78,61 +83,23 @@ export default createAppConfig(
 											// This means we only store one document: the main entry page
 											return '/apps/collectives/'
 										},
-										handlerDidError: async () => {
-											// If network fails, respond with cached app entrypoint
-											const cache = await self.caches.open('collectives-vue-router')
-											return await cache.match('/apps/collectives/') || Response.error()
-										},
 									}],
 								},
 							},
-							// Cache for any non-navigation requests
+
 							{
+								// Cache for other apps assets
+
 								// Strategy: try network first, then cache
 								handler: 'NetworkFirst',
 								// Cache assets and API requests
 								// urlPattern: /^.*/,
-								urlPattern: ({ request }) => request.mode !== 'navigate',
+								urlPattern: /.*\.(js|css|mjs|webp|svg)$/,
 								options: {
-									cacheName: 'collectives-runtime',
-									// Cache max 10000 requests for one week
+									cacheName: 'collectives-external-assets',
+									// Cache for one week
 									expiration: {
 										maxAgeSeconds: 3600 * 24 * 7, // one week
-										maxEntries: 10000,
-									},
-								},
-							},
-							// Queue outgoing Text API POST requests
-							// TODO: test
-							{
-								// Strategy: network only
-								handler: 'NetworkOnly',
-								// Cache Text API requests
-								urlPattern: /^.*\/apps\/text\/session\/.*/,
-								method: 'POST',
-								options: {
-									backgroundSync: {
-										name: 'textApiPostQueue',
-										options: {
-											maxRetentionTime: 3600, // one hour
-										},
-									},
-								},
-							},
-							// Queue outgoing Text API PUT requests
-							// TODO: test
-							{
-								// Strategy: network only
-								handler: 'NetworkOnly',
-								// Cache Text API requests
-								urlPattern: /^.*\/apps\/text\/session\/.*/,
-								method: 'PUT',
-								options: {
-									backgroundSync: {
-										name: 'textApiPutQueue',
-										options: {
-											maxRetentionTime: 3600, // one hour
-										},
 									},
 								},
 							},
