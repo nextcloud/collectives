f2763fa77d0297b900f97b6064d2a7ebb016869d
Max <max@nextcloud.com>
2025-09-13 16:41:20 +0200

    enh(service-worker): use prefix from url for precache
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/service-worker.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
+
+declare let self: ServiceWorkerGlobalScope
+
+// self.__WB_MANIFEST is default injection point
+const manifest = self.__WB_MANIFEST
+
+const registrationUrl = new URL(location.href);
+const prefix = registrationUrl.searchParams.get('prefix');
+
+const itemsToPrecache = manifest.map(item => {
+    if (typeof item === 'string') {
+        return prefix + item
+    } else {
+        item.url = prefix + item.url
+        return item;
+    }
+});
+
+precacheAndRoute(itemsToPrecache)
+
+// clean old assets
+cleanupOutdatedCaches()
+
+self.skipWaiting()
--- vite.config.js
+++ vite.config.js
@@ -35,78 +35,18 @@ export default createAppConfig(
 					manifest: false,
 					// Don't try to inject service worker registration. We do it manually at Collectives.vue
 					injectRegister: false,
-					// matching nextcloud-vite-config
 					outDir: './',
-					filename: 'js/collectives-service-worker.js',
 					// Enable service worker in development build
 					devOptions: { enabled: true },
-
-					workbox: {
-						// Attempt to identify and delete any precaches created by older, incompatible versions
-						clientsClaim: true,
-						// Include runtime code for the Workbox library in the top-level service worker
-						inlineWorkboxRuntime: true,
-						// Let all clients use the new service worker version immediately after update
-						skipWaiting: true,
-						// Precache for collectives app assets
+					strategies: 'injectManifest',
+					srcDir: 'src',
+					filename: 'service-worker.js',
+					injectManifest: {
 						globPatterns: [
-							"js/*.mjs",
-							"css/*.css",
+							 "css/*.css",
+							 "js/*.mjs",
 						],
 						maximumFileSizeToCacheInBytes: 5242880,
-
-						// Disable navigateFallback (required as precache seems to auto-enable it)
-						navigateFallback: null,
-
-						// Don't create a sourcemap for the service worker files
-						// sourcemap: false,
-
-						// Configure cache responses for runtime routes
-						runtimeCaching: [
-							{
-								// Cache for navigation requests within the vue router scope
-
-								// Strategy: try network first, then cache
-								handler: 'NetworkFirst',
-								// Cache assets and API requests
-								// urlPattern: /^.*/,
-								// urlPattern: /(?:\/index\.php)?\/apps\/collectives/,
-								urlPattern: ({ url, request }) => request.mode === 'navigate'
-									&& url.pathname.match(/(?:\/index\.php)?\/apps\/collectives/),
-
-								options: {
-									cacheName: 'collectives-vue-router',
-									// Cache max for one week
-									expiration: {
-										maxAgeSeconds: 3600 * 24 * 7, // one week
-									},
-									plugins: [{
-										cacheKeyWillBeUsed: async () => {
-											// Always use same cache key (apps entrypoint) for vue-router URLs
-											// This means we only store one document: the main entry page
-											return '/apps/collectives/'
-										},
-									}],
-								},
-							},
-
-							{
-								// Cache for other apps assets
-
-								// Strategy: try network first, then cache
-								handler: 'NetworkFirst',
-								// Cache assets and API requests
-								// urlPattern: /^.*/,
-								urlPattern: /.*\.(js|css|mjs|webp|svg)$/,
-								options: {
-									cacheName: 'collectives-external-assets',
-									// Cache for one week
-									expiration: {
-										maxAgeSeconds: 3600 * 24 * 7, // one week
-									},
-								},
-							},
-						],
 					},
 				})
 			],
