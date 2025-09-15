8098963f9b8bd6416256ae84a6a2007e45c37f3d
Jonas <jonas@freesources.org>
2025-09-15 14:30:23 +0200

    feat(serviceWorker): Bring back runtime caching
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { clientsClaim } from 'workbox-core'
+import { clientsClaim, type RouteMatchCallbackOptions } from 'workbox-core'
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
+import { registerRoute } from 'workbox-routing'
+import { NetworkFirst } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -27,6 +30,32 @@ precacheAndRoute(itemsToPrecache)
 
 // clean old assets
 cleanupOutdatedCaches()
+
+// Cache for navigation requests within the vue router scope
+const matchNavigateCb = ({ url, request }: RouteMatchCallbackOptions) => {
+	return request.mode === 'navigate' && url.pathname.match(/(?:\/index.php)?\/apps\/collectives/)
+}
+registerRoute(matchNavigateCb, new NetworkFirst({
+	cacheName: 'collectives-vue-router',
+	plugins: [
+		new ExpirationPlugin({
+			maxAgeSeconds: 3600 * 24 * 7, // one week
+		}),
+		{
+			cacheKeyWillBeUsed: async () => '/apps/collectives',
+		},
+	],
+}))
+
+// Cache for assets from other apps
+registerRoute(/.*\.(css|js|mjs|svg|webp)$/, new NetworkFirst({
+	cacheName: 'collectives-assets',
+	plugins: [
+		new ExpirationPlugin({
+			maxAgeSeconds: 3600 * 24 * 7, // one week
+		}),
+	],
+}))
 
 self.skipWaiting()
 clientsClaim()
