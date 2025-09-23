43b23f4b4723a56691a8bdc9c0960ef1f2546f67
Jonas <jonas@freesources.org>
2025-09-23 15:14:20 +0200

    feat(networkState): networkState composable + OfflineContent component
    Signed-off-by: Jonas <jonas@freesources.org>

---
+++ src/components/PageSidebar/OfflineContent.vue
@@ -0,0 +1,28 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<NcEmptyContent :name="t('collectives', 'Offline')"
+		:description="t('collectives', 'Make sure you are online and can reach the server.')">
+		<template #icon>
+			<CloudOffOutlineIcon />
+		</template>
+	</NcEmptyContent>
+</template>
+
+<script lang="ts">
+import { NcEmptyContent } from '@nextcloud/vue'
+import { defineComponent } from 'vue'
+import CloudOffOutlineIcon from 'vue-material-design-icons/CloudOffOutline.vue'
+
+export default defineComponent({
+	name: 'OfflineContent',
+
+	components: {
+		CloudOffOutlineIcon,
+		NcEmptyContent,
+	},
+})
+</script>
