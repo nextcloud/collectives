92325ef58b62c6443fd8dd950334b974724a873a
Jonas <jonas@freesources.org>
2025-09-23 18:33:50 +0200

    feat: Add offline state indicator
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/components/Collective.vue
+++ src/components/Collective.vue
@@ -11,6 +11,27 @@
 		<PageVersion v-else-if="currentPage && selectedVersion" />
 		<Page v-else-if="currentPage" />
 		<PageNotFound v-else />
+
+		<NcPopover v-if="!networkOnline"
+			:aria-label="t('collectives', 'Offline')"
+			:auto-hide="false"
+			no-focus-trap
+			class="offline-indicator">
+			<template #trigger>
+				<NcButton type="tertiary"
+					:aria-label="t('collectives', 'Offline')"
+					class="trigger offline-indicator__button"
+					:class="{'mobile': isMobile, 'desktop': !isMobile }">
+					<template #icon>
+						<span class="offline-indicator__dot" />
+					</template>
+					{{ offlineIndicatorText }}
+				</NcButton>
+			</template>
+			<div class="offline-indicator__hint">
+				<span>{{ t('collectives', 'Offline') }}</span>
+			</div>
+		</NcPopover>
 	</NcAppContentDetails>
 </template>
 
@@ -23,22 +44,32 @@ import { useTagsStore } from '../stores/tags.js'
 import { usePagesStore } from '../stores/pages.js'
 import { useVersionsStore } from '../stores/versions.js'
 import { emit } from '@nextcloud/event-bus'
-import { NcAppContentDetails } from '@nextcloud/vue'
+import { NcAppContentDetails, NcButton, NcPopover } from '@nextcloud/vue'
+import { useIsMobile } from '@nextcloud/vue/composables/useIsMobile'
 import displayError from '../util/displayError.js'
 import Page from './Page.vue'
 import PageVersion from './PageVersion.vue'
 import PageNotFound from './Page/PageNotFound.vue'
 import SkeletonLoading from './SkeletonLoading.vue'
+import { useNetworkState } from '../composables/useNetworkState.ts'
 
 export default {
 	name: 'Collective',
 
 	components: {
-		SkeletonLoading,
 		NcAppContentDetails,
+		NcButton,
+		NcPopover,
 		Page,
 		PageNotFound,
 		PageVersion,
+		SkeletonLoading,
+	},
+
+	setup() {
+		const isMobile = useIsMobile()
+		const { networkOnline } = useNetworkState()
+		return { isMobile, networkOnline }
 	},
 
 	computed: {
@@ -59,6 +90,12 @@ export default {
 
 		notFound() {
 			return !this.loading('pagelist') && !this.loading('currentPage') && !this.currentPage
+		},
+
+		offlineIndicatorText() {
+			return this.isMobile
+				? ''
+				: t('collectives', 'Offline')
 		},
 	},
 
@@ -120,6 +157,45 @@ export default {
 
 }
 </script>
+
+<style scoped lang="scss">
+.offline-indicator {
+	position: absolute;
+	z-index: 100010;
+	bottom: calc(var(--default-grid-baseline) * 3);
+	margin-left: calc(var(--default-grid-baseline) * 3);
+	background-color: var(--color-main-background);
+	border-radius: var(--border-radius-element);
+
+	&__button {
+		border: 1px solid var(--color-border) !important;
+
+		&.mobile {
+			// Same padding left and right on mobile without text
+			padding-inline: var(--default-grid-baseline);
+		}
+
+		&.desktop {
+			// No tooltip on desktop
+			pointer-events: none;
+		}
+	}
+
+	&__dot {
+		display: inline-block;
+		height: 14px;
+		width: 14px;
+		background-color: var(--color-element-error, var(--color-error));
+		border-radius: 50%;
+	}
+
+	&__hint {
+		padding: 12px;
+		max-width: 300px;
+		text-align: start;
+	}
+}
+</style>
 
 <style lang="scss">
 .app-content-details {
