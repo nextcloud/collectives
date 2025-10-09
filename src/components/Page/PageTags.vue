cac90af2460e12d1d2bd291563d932950f12e28d
Jonas <jonas@freesources.org>
2025-10-09 10:04:21 +0200

    fix(PageTags): no focus trap for popover
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/components/Page/PageTags.vue
+++ src/components/Page/PageTags.vue
@@ -23,7 +23,7 @@
 				:tag="tag"
 				@select="onSelectTag(tag.id)" />
 
-			<NcPopover v-if="pageTagsInvisible.length > 0" popup-role="listbox">
+			<NcPopover v-if="pageTagsInvisible.length > 0" popup-role="listbox" no-focus-trap>
 				<template #trigger="{ attrs }">
 					<PageTag
 						v-bind="attrs"
