922529e27ffce5d18ee0b0f80ea5b08d4357b4cd
Jonas <jonas@freesources.org>
2025-11-04 15:33:45 +0100

    feat(NodeHelper): Functions to extract links and linked pageIds from markdown
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Fs/NodeHelper.php
+++ lib/Fs/NodeHelper.php
@@ -9,6 +9,15 @@ declare(strict_types=1);
 
 namespace OCA\Collectives\Fs;
 
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Exception\CommonMarkException;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
+use League\CommonMark\Node\Inline\Text;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Parser\MarkdownParser;
+use OC;
+use OCA\Collectives\Db\Collective;
 use OCA\Collectives\Model\PageInfo;
 use OCA\Collectives\Service\NotFoundException;
 use OCA\Collectives\Service\NotPermittedException;
@@ -250,5 +259,123 @@ class NodeHelper {
 		}
 
 		return 0;
+	}
+
+	/**
+	 * Recursively collect text from a CommonMark node and its descendants
+	 */
+	private static function collectText(Node $node): string {
+		if ($node instanceof Text) {
+			return $node->getLiteral();
+		}
+
+		$out = '';
+		foreach ($node->children() as $child) {
+			$out .= self::collectText($child);
+		}
+
+		return $out;
+	}
+
+	/**
+	 * @throws CommonMarkException
+	 */
+	public static function getLinksFromContent(string $content): array {
+		$environment = new Environment();
+		$environment->addExtension(new CommonMarkCoreExtension());
+		$parser = new MarkdownParser($environment);
+		$document = $parser->parse($content);
+		$walker = $document->walker();
+
+		$links = [];
+		while ($event = $walker->next()) {
+			if (! $event->isEntering()) {
+				continue;
+			}
+			$node = $event->getNode();
+			if (!($node instanceof Link)) {
+				continue;
+			}
+
+			$textParts = [];
+			foreach ($node->children() as $child) {
+				$textParts[] = self::collectText($child);
+			}
+			$text = trim(implode('', array_filter($textParts)));
+
+			$links[] = [
+				'text' => $text,
+				'href' => $node->getUrl(),
+				'title' => $node->getTitle() ?? '',
+			];
+		}
+
+		return $links;
+	}
+
+	/**
+	 * @throws CommonMarkException
+	 */
+	public static function getLinkedPageIds(Collective $collective, string $content, array $trustedDomains = []): array {
+		$links = self::getLinksFromContent($content);
+
+		// Absolute URL regex
+		$protocol = '^https?:\/\/';
+		$trustedDomainsArray = array_map(static fn (string $domain) => str_replace('\*', '\w*', preg_quote($domain, '/')), $trustedDomains);
+		$trustedDomainsPart = $trustedDomainsArray !== [] ? '(?:' . implode('|', $trustedDomainsArray) . ')' : 'localhost';
+		$absoluteUrlRegex = '/' . $protocol . $trustedDomainsPart . '(?::[0-9]+)?(.+)$/';
+
+		// Root-relative URL regex
+		$ocWebrootPath = OC::$WEBROOT ? '\/+' . preg_quote(str_replace('/', '/+', trim(OC::$WEBROOT, '/')), '/') : '';
+		$basePath = $ocWebrootPath . '(?:\/+index\.php)?';
+		$appPath = '\/+apps\/+collectives\/+';
+		$collectivePath = '(?:' . implode('|', [
+			// slug may only contain ascii characters
+			'[\x00-\x7F]+\-' . $collective->getId(),
+			rawurlencode($collective->getName()),
+		]) . ')(?=\/+)';
+		$rootRelativeUrlRegex = '/^' . $basePath . $appPath . $collectivePath . '(.+)$/';
+
+		// slug may only contain ascii characters
+		$slugFragmentSuffix = '(?:[?#].*)?$/';
+		$slugPagePathRegex = '/\/[\x00-\x7F]+\-([0-9]+)' . $slugFragmentSuffix;
+		$noSlugFragmentSuffix = '(?:[&#].*)?$/';
+		$noSlugPagePathRegex = '/^.*[^?]+\?fileId=([0-9]+)' . $noSlugFragmentSuffix;
+
+		$pageIds = [];
+		foreach ($links as $link) {
+			$href = $link['href'];
+			if (!$href) {
+				continue;
+			}
+
+			// Absolute URL (with protocol)
+			if (preg_match('/^[a-zA-Z]+:\/\//', $href)) {
+				// absolute link
+				if (!preg_match($absoluteUrlRegex, $href, $matches)) {
+					// Absolute link doesn't point to Nextcloud instance
+					continue;
+				}
+				$href = $matches[1];
+			}
+
+			// Root-relative URL
+			if (str_starts_with($href, '/')) {
+				if (preg_match($rootRelativeUrlRegex, $href, $matches)) {
+					$href = $matches[1];
+				} else {
+					// Root-relative URL doesn't point to the collective
+					continue;
+				}
+			}
+
+			if (preg_match($slugPagePathRegex, $href, $matches)) {
+				$pageIds[] = (int)$matches[1];
+			} elseif (preg_match($noSlugPagePathRegex, $href, $matches)) {
+				$pageIds[] = (int)$matches[1];
+			}
+		}
+
+		return array_unique($pageIds, SORT_NUMERIC);
 	}
 }
