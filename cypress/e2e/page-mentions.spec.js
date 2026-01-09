8b272b2423342f2fd46b4e810d067b5642284bee
Jonas <jonas@freesources.org>
2026-01-09 13:08:09 +0100

    chore: Remove support for Nextcloud 30
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/e2e/page-mentions.spec.js
+++ cypress/e2e/page-mentions.spec.js
@@ -4,37 +4,35 @@
  */
 
 describe('Page mentions', function() {
-	if (!['stable30'].includes(Cypress.env('ncVersion'))) {
-		before(function() {
-			cy.loginAs('bob')
-			cy.deleteAndSeedCollective('Mention Collective')
-				.seedPage('Page 1', '', 'Readme.md')
-			cy.circleFind('Mention Collective')
-				.circleAddMember('alice')
+	before(function() {
+		cy.loginAs('bob')
+		cy.deleteAndSeedCollective('Mention Collective')
+			.seedPage('Page 1', '', 'Readme.md')
+		cy.circleFind('Mention Collective')
+			.circleAddMember('alice')
 
-			cy.visit('/apps/collectives/Mention Collective/Page 1')
-			cy.getEditorContent(true)
-				.type('Bob mentions @alice')
-			cy.get('.tippy-content')
-				.contains('alice')
+		cy.visit('/apps/collectives/Mention Collective/Page 1')
+		cy.getEditorContent(true)
+			.type('Bob mentions @alice')
+		cy.get('.tippy-content')
+			.contains('alice')
+			.click()
+		cy.getEditor()
+			.find('.save-status')
+			.click()
+	})
+
+	describe('Mentioning another member in a page', function() {
+		it('Notification is shown to mentioned member', function() {
+			cy.loginAs('alice')
+			cy.visit('/apps/collectives/Mention Collective')
+			cy.get('.notifications-button')
 				.click()
-			cy.getEditor()
-				.find('.save-status')
-				.click()
-		})
 
-		describe('Mentioning another member in a page', function() {
-			it('Notification is shown to mentioned member', function() {
-				cy.loginAs('alice')
-				cy.visit('/apps/collectives/Mention Collective')
-				cy.get('.notifications-button')
-					.click()
-
-				cy.get('#header-menu-notifications')
-					.find('.notification .rich-text--wrapper')
-					.invoke('text')
-					.should('match', /bob \s*has mentioned you in .*Mention Collective - .*Page 1/)
-			})
+			cy.get('#header-menu-notifications')
+				.find('.notification .rich-text--wrapper')
+				.invoke('text')
+				.should('match', /bob \s*has mentioned you in .*Mention Collective - .*Page 1/)
 		})
-	}
+	})
 })
