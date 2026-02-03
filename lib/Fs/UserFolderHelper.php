5ba1fac3b97ce39676c2cb762f95f478f7f715ea
Jonas <jonas@freesources.org>
2026-02-03 14:40:35 +0100

    chore(UserFolderHelper): remove superfluous folder caching
    No longer needed now that the user folder is a mountpoint and conflict
    resolution happens in the MountProvider.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Fs/UserFolderHelper.php
+++ lib/Fs/UserFolderHelper.php
@@ -9,24 +9,15 @@ declare(strict_types=1);
 
 namespace OCA\Collectives\Fs;
 
-use OC\User\NoUserException;
-use OCA\Collectives\Service\NotFoundException;
-use OCA\Collectives\Service\NotPermittedException;
 use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
 use OCP\Files\NotFoundException as FilesNotFoundException;
-use OCP\Files\NotPermittedException as FilesNotPermittedException;
 use OCP\IAppConfig;
 use OCP\IConfig;
 use OCP\IUserManager;
 use OCP\L10N\IFactory;
-use OCP\PreConditionNotMetException;
-use UnexpectedValueException;
 
 class UserFolderHelper {
-	private ?Folder $userCollectivesFolder = null;
-	private ?string $initializedUser = null;
-
 	public function __construct(
 		private IRootFolder $rootFolder,
 		private IUserManager $userManager,
@@ -36,9 +27,6 @@ class UserFolderHelper {
 	) {
 	}
 
-	/**
-	 * @throws NotPermittedException
-	 */
 	public function getUserFolderSetting(string $userId): string {
 		$defaultUserFolder = $this->appConfig->getValueString('collectives', 'default_user_folder', '');
 		// Get collectives user folder from settings and default to translated 'Collectives'
@@ -48,53 +36,20 @@ class UserFolderHelper {
 			$userLang = $this->l10nFactory->getUserLanguage($user);
 			$l10n = $this->l10nFactory->get('collectives', $userLang);
 			$userCollectivesPath = '/' . $l10n->t('Collectives');
-			try {
-				$this->config->setUserValue($userId, 'collectives', 'user_folder', $userCollectivesPath);
-			} catch (PreConditionNotMetException|UnexpectedValueException $e) {
-				throw new NotPermittedException($e->getMessage(), 0, $e);
-			}
+			$this->config->setUserValue($userId, 'collectives', 'user_folder', $userCollectivesPath);
 		}
 
 		return $userCollectivesPath;
 	}
 
-	/**
-	 * @throws NotPermittedException
-	 */
-	public function getUserRootFolder(string $userId): Folder {
-		try {
-			return $this->rootFolder->getUserFolder($userId);
-		} catch (FilesNotPermittedException|NoUserException $e) {
-			throw new NotPermittedException($e->getMessage(), 0, $e);
-		}
-	}
-
-	/**
-	 * @throws NotPermittedException
-	 * @throws NotFoundException
-	 */
-	private function cache(string $userId): Folder {
-		$userRootFolder = $this->getUserRootFolder($userId);
+	public function get(string $userId): Folder {
+		$userHomeFolder = $this->rootFolder->getUserFolder($userId);
 		$userCollectivesPath = $this->getUserFolderSetting($userId);
-
-		$userCollectivesFolder = $userRootFolder->get($userCollectivesPath);
+		$userCollectivesFolder = $userHomeFolder->get($userCollectivesPath);
 		if (!$userCollectivesFolder instanceof Folder) {
-			throw new NotFoundException('Collectives path exists but is not a folder');
+			throw new FilesNotFoundException('Collectives path exists but is not a folder');
 		}
 
 		return $userCollectivesFolder;
-	}
-
-	/**
-	 * @throws NotPermittedException
-	 * @throws NotFoundException
-	 */
-	public function get(string $userId): Folder {
-		if (!$this->userCollectivesFolder || $userId !== $this->initializedUser) {
-			$this->userCollectivesFolder = $this->cache($userId);
-			$this->initializedUser = $userId;
-		}
-
-		return $this->userCollectivesFolder;
 	}
 }
