e3981812920f47420db1b3d94fabb11cbcad1255
Ferdinand Thiessen <opensource@fthiessen.de>
2026-02-11 12:49:24 +0100

    fix(files): use `@nextcloud/files` v4 for Nextcloud 33+
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/init.js
+++ src/init.js
@@ -3,7 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { registerFileListHeaders } from '@nextcloud/files'
+import { registerFileListHeader } from '@nextcloud/files'
+import { registerFileListHeaders as legacyRegisterFileListHeader } from '@nextcloud/files-legacy'
 import { FilesCollectiveHeader } from './files/FilesCollectiveHeader.js'
 
-registerFileListHeaders(FilesCollectiveHeader)
+const version = Number.parseInt((window.OC?.config?.version ?? '0').split('.')[0])
+
+if (version >= 33) {
+	registerFileListHeader(FilesCollectiveHeader)
+} else {
+	legacyRegisterFileListHeader(FilesCollectiveHeader)
+}
