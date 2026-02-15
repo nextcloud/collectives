9ceb664d3576b97fc5a9d81c1419fd31c17e269b
Jonas <jonas@freesources.org>
2026-02-15 11:18:16 +0100

    test(playwright): refactor fixture to allow creating multiple collectives
    Signed-off-by: Jonas <jonas@freesources.org>

---
+++ playwright/support/fixtures/create-collectives.ts
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { type Collective } from './Collective.ts'
+import { test as base } from './random-user.ts'
+
+export interface CollectivesFixture {
+	collectiveConfigs: Array<{ name: string, emoji?: string }>
+	collectives: Collective[]
+	collective: Collective
+}
+
+/**
+ * This test fixture creates multiple collectives for the user and makes them available for the test.
+ *
+ * To customize collectives, extend this fixture in your test file:
+ * ```
+ * const test = createCollectiveTest.extend<{}>({
+ *   collectiveConfigs: async ({}, use) => use([
+ *     { name: 'Custom 1', emoji: '🎯' },
+ *     { name: 'Custom 2', emoji: '🚀' },
+ *   ]),
+ * })
+ *
+ * test('My test', async ({ collectives }) => {
+ *   // collectives[0].name === 'Custom 1'
+ * })
+ * ```
+ */
+export const test = base.extend<CollectivesFixture>({
+	// eslint-disable-next-line no-empty-pattern
+	collectiveConfigs: async ({}, use) => {
+		await use([
+			{ name: 'Test Collective 1', emoji: '🌟' },
+		])
+	},
+	collectives: async ({ collectiveConfigs, user }, use) => {
+		const createdCollectives: Collective[] = []
+
+		// Create all collectives
+		for (const config of collectiveConfigs) {
+			const collective = await user.createCollective(config)
+			createdCollectives.push(collective)
+		}
+
+		await use(createdCollectives)
+
+		// Cleanup all collectives
+		for (const collective of createdCollectives) {
+			await user.deleteCollective({ id: collective.id })
+		}
+	},
+	collective: async ({ collectives }, use) => {
+		await use(collectives[0])
+	},
+})
