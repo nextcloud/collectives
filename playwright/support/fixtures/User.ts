669a9686f88b9c7c44c1a134ea68a473f301aded
Jonas <jonas@freesources.org>
2026-02-23 16:52:24 +0100

    test(playwright): make sure we always send authenticated requests
    Signed-off-by: Jonas <jonas@freesources.org>

--- playwright/support/fixtures/User.ts
+++ playwright/support/fixtures/User.ts
@@ -3,13 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import { type User as Account } from '@nextcloud/e2e-test-server'
 import { type Page } from '@playwright/test'
 import { createCollective, trashAndDeleteCollective } from './Collective.ts'
 
 export class User {
 	constructor(
+		public readonly account: Account,
 		public readonly page: Page,
-		public readonly userId: string,
 	) {
 	}
 
