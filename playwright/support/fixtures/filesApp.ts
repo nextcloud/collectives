83780977923b03f0705f5a95a28d18180dd0af7a
Jonas <jonas@freesources.org>
2026-02-23 13:36:39 +0100

    test(playwright): refactor files app fixture
    Signed-off-by: Jonas <jonas@freesources.org>

--- playwright/support/fixtures/filesApp.ts
+++ playwright/support/fixtures/filesApp.ts
@@ -3,25 +3,16 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { type Locator } from '@playwright/test'
 import { test as baseTest } from '@playwright/test'
+import { FilesAppSection } from '../sections/FilesAppSection.ts'
 
 type FilesAppFixture = {
-	openFilesApp: () => Promise<void>
-	getFileListEntry: (fileName: string) => Locator
-	openFile: (fileName: string) => Promise<void>
+	filesApp: FilesAppSection
 }
 
 export const test = baseTest.extend<FilesAppFixture>({
-	openFilesApp: ({ page }, use) => use(async () => {
-		await page.goto('/apps/files/')
-	}),
-	getFileListEntry: ({ page }, use) => use((fileName: string) => {
-		return page.locator(`[data-cy-files-list-row-name="${fileName}"]`)
-	}),
-	openFile: ({ page }, use) => use(async (fileName: string) => {
-		// Open the file by clicking on it in the file list
-		const fileEntry = page.locator(`[data-cy-files-list-row-name="${fileName}"]`)
-		await fileEntry.click()
-	}),
+	filesApp: async ({ page }, use) => {
+		const filesApp = new FilesAppSection(page)
+		await use(filesApp)
+	},
 })
