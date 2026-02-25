6d19b2afbb43b3bf9c1ac5695e65ea60dfe2efef
Jonas <jonas@freesources.org>
2026-02-25 13:01:14 +0100

    fix(PublicStartController): Session bag expects array of IDs
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/mixins/pageContentMixin.js
+++ src/mixins/pageContentMixin.js
@@ -17,7 +17,7 @@ export default {
 
 	methods: {
 		/**
-		 * Get markdown content of page
+		 * Get Markdown content of page
 		 *
 		 * @param {string} davUrl URL to fetch page via DAV
 		 */
@@ -28,16 +28,10 @@ export default {
 					timestamp: Math.floor(Date.now() / 1000),
 				},
 			}
-			// Authenticate via share token for public shares
-			if (this.isPublic) {
-				axiosConfig.auth = {
-					username: this.shareTokenParam,
-				}
-			}
 
 			try {
 				const content = await axios.get(davUrl, axiosConfig)
-				// content.data will attempt to parse as json, but we want the raw text.
+				// content.data will attempt to parse as JSON, but we want the raw text.
 				return content.request.responseText
 			} catch (e) {
 				console.error('Failed to fetch content of page', e)
