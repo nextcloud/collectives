66de16b6cf4253149ee6091950d3f2343a52c617
Jonas <jonas@freesources.org>
2026-03-21 14:52:34 +0100

    fix(ImportDirectory): really prevent path traversal in processAttachment
    Also add a unit test for ImportService happy path and one for detecting
    path traversal in attachments path.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $nextcloud_source = getenv('NEXTCLOUD_SOURCE')
   ?: __DIR__ . '/../../..';
 require_once $nextcloud_source . '/tests/bootstrap.php';
