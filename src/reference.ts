3f9273168127f23b201a25434c7c2c80022765d8
Jonas <jonas@freesources.org>
2026-04-01 16:11:53 +0200

    feat: add basic custom page picker for smart picker
    Signed-off-by: Jonas <jonas@freesources.org>

---
+++ src/reference.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { App } from 'vue'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { registerCustomPickerElement, registerWidget } from '@nextcloud/vue/components/NcRichText'
+import PagePicker from './views/PagePicker.vue'
+import PageReferenceWidget from './views/PageReferenceWidget.vue'
+
+registerWidget('collectives_page', (el, { richObjectType, richObject, accessible }) => {
+	createApp(PageReferenceWidget, {
+		richObjectType,
+		richObject,
+		accessible,
+	}).mount(el)
+})
+
+registerCustomPickerElement('collectives-ref-pages', (el, { providerId, accessible }) => {
+	const pinia = createPinia()
+	const app = createApp(PagePicker, { providerId, accessible })
+	app.use(pinia)
+	app.mount(el)
+	return app
+}, (el, renderResult) => {
+	(renderResult.object as App)?.unmount()
+}, 'normal')
