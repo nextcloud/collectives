897d6b7367c81fb7085df8ab3477f2ac5d44386d
Jonas <jonas@freesources.org>
2026-04-16 17:59:38 +0200

    chore(PageReferenceWidget): move PagePreview into own component
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/views/PageReferenceWidget.vue
+++ src/views/PageReferenceWidget.vue
@@ -4,75 +4,26 @@
 -->
 
 <template>
-	<!-- handle inaccessible pages -->
-	<a
-		v-if="richObject && notFound"
-		:href="richObject.link"
-		target="_blank"
-		class="collective-page not-found">
-		<div class="collective-page--image">
-			<PageIcon
-				:size="50" />
-		</div>
-		<div class="collective-page--info">
-			<div class="title">
-				<strong>
-					{{ t('collectives', 'Page not found') }}
-				</strong>
-			</div>
-			<div class="description">
-				{{ t('collectives', 'The page does not exist or you are not allowed to view it.') }}
-			</div>
-		</div>
-	</a>
-	<!-- handle accessible pages -->
-	<a
-		v-else-if="richObject"
-		:href="richObject.link"
-		target="_blank"
-		class="collective-page"
-		@click="clickLink">
-		<div class="collective-page--image">
-			<span
-				v-if="emoji"
-				class="page-emoji">
-				{{ emoji }}
-			</span>
-			<PageIcon
-				v-else
-				:size="50" />
-		</div>
-		<div class="collective-page--info">
-			<div class="title">
-				<strong>
-					{{ richObject.page.title }}
-				</strong>
-			</div>
-			<div class="description">
-				{{ richObject.description }}
-			</div>
-			<div class="last-edited">
-				{{ richObject.lastEdited }}
-				<NcUserBubble
-					:user="richObject.page.lastUserId"
-					:displayName="richObject.page.lastUserDisplayName" />
-			</div>
-		</div>
-	</a>
+	<PagePreview
+		v-if="richObject"
+		:notFound
+		:link="richObject.link"
+		:title="richObject.page?.title || ''"
+		:description="richObject.description"
+		:emoji="richObject.page?.emoji"
+		:lastEdited="richObject.lastEdited"
+		:lastUserId="richObject.page?.lastUserId"
+		:lastUserDisplayName="richObject.page?.lastUserDisplayName" />
 </template>
 
 <script>
-import { t } from '@nextcloud/l10n'
-import { generateUrl } from '@nextcloud/router'
-import NcUserBubble from '@nextcloud/vue/components/NcUserBubble'
-import PageIcon from '../components/Icon/PageIcon.vue'
+import PagePreview from '../components/PagePreview.vue'
 
 export default {
 	name: 'PageReferenceWidget',
 
 	components: {
-		PageIcon,
-		NcUserBubble,
+		PagePreview,
 	},
 
 	/* eslint-disable vue/no-unused-properties */
@@ -95,63 +46,9 @@ export default {
 	},
 
 	computed: {
-		emoji() {
-			return this.richObject.page?.emoji
-		},
-
 		notFound() {
 			return this.accessible === false
 		},
 	},
-
-	methods: {
-		t,
-
-		clickLink(event) {
-			const appUrl = '/apps/collectives'
-			const linkUrl = new URL(this.richObject.link, window.location)
-			// Only consider rerouting if we're inside the collectives app and for links to collectives app
-			if (window.OCA.Collectives?.vueRouter
-				&& linkUrl.pathname.toString().startsWith(generateUrl(appUrl))) {
-				event.preventDefault()
-				const collectivesUrl = linkUrl.href.substring(linkUrl.href.indexOf(appUrl) + appUrl.length)
-				window.OCA.Collectives.vueRouter.push(collectivesUrl)
-			}
-		},
-	},
 }
 </script>
-
-<style scoped lang="scss">
-.collective-page {
-	width: 100%;
-	white-space: normal;
-	padding: 12px !important;
-	display: flex;
-	text-decoration: unset !important;
-	color: var(--color-main-text) !important;
-
-	&--image {
-		margin-inline-end: 12px;
-		display: flex;
-		align-items: center;
-		.page-emoji {
-			display: flex;
-			align-items: center;
-			height: 50px;
-			font-size: 50px;
-		}
-	}
-
-	&.not-found {
-		.description,
-		.collective-page--image {
-			color: var(--color-text-maxcontrast) !important
-		}
-	}
-
-	.spacer {
-		flex-grow: 1;
-	}
-}
-</style>
