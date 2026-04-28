f3deeb72dc1175b0889441b1d1a2e6cdbbb464c9
Jonas <jonas@freesources.org>
2026-04-28 10:03:17 +0200

    test(cypress): fix enabling apps with hardened server API endpoint
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -134,9 +134,15 @@ Cypress.Commands.add('disableApp', (appName) => {
 Cypress.Commands.add('setAppEnabled', (appName, value = true) => {
 	const verb = value ? 'enable' : 'disable'
 	const url = `${Cypress.env('baseUrl')}/index.php/settings/apps/${verb}`
+
+	const headers = {}
+	if (verb === 'enable') {
+		headers.Authorization = `Basic ${btoa('admin:admin')}`
+	}
 	return axios.post(
 		url,
 		{ appIds: [appName] },
+		{ headers },
 	)
 })
 
