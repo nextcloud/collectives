636b0dc5b1e29b667ad3141de62880d7715526b9
Jonas <jonas@freesources.org>
2026-05-28 16:51:40 +0200

    test(playwright): add tests for notifications feature
    Requires to install composer in Docker container as notifications app
    has run-time PHP dependencies.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- playwright/support/fixtures/random-user.ts
+++ playwright/support/fixtures/random-user.ts
@@ -4,6 +4,7 @@
  */
 
 import type { User as Account } from '@nextcloud/e2e-test-server'
+import type { Browser, Page } from '@playwright/test'
 
 import { createRandomUser, login } from '@nextcloud/e2e-test-server/playwright'
 import { test as base } from '@playwright/test'
@@ -12,6 +13,23 @@ import { User } from './User.ts'
 export interface UserFixture {
 	account: Account
 	user: User
+}
+
+/**
+ * Log in as any account in a new browser page and return it.
+ *
+ * @param browser - the browser object
+ * @param baseURL - the base URL
+ * @param account - the user account
+ */
+export async function loginAsUser(browser: Browser, baseURL: string | undefined, account: Account): Promise<Page> {
+	// Important: make sure we authenticate in a clean environment by unsetting storage state.
+	const page = await browser.newPage({ storageState: undefined, baseURL })
+	await login(page.request, account)
+	const tokenResponse = await page.request.get('./csrftoken', { failOnStatusCode: true })
+	const { token } = (await tokenResponse.json()) as { token: string }
+	await page.context().setExtraHTTPHeaders({ requesttoken: token })
+	return page
 }
 
 /**
@@ -24,19 +42,7 @@ export const test = base.extend<object, UserFixture>({
 		await use(account)
 	}, { scope: 'worker' }],
 	page: async ({ account, browser, baseURL }, use) => {
-		// Important: make sure we authenticate in a clean environment by unsetting storage state.
-		const page = await browser.newPage({
-			storageState: undefined,
-			baseURL,
-		})
-
-		await login(page.request, account)
-		const tokenResponse = await page.request.get('./csrftoken', {
-			failOnStatusCode: true,
-		})
-		const { token } = (await tokenResponse.json()) as { token: string }
-		await page.context().setExtraHTTPHeaders({ requesttoken: token })
-
+		const page = await loginAsUser(browser, baseURL, account)
 		await use(page)
 		await page.close()
 	},
