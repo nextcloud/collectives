1f0d6218e92dd9deffe51d64163e87629f5769ad
ZakariaHogeschoolR <ztahiri903@gmail.com>
2026-08-29 23:30:28 +0200

    fix: correct import order in root store
    Signed-off-by: ZakariaHogeschoolR <ztahiri903@gmail.com>

--- src/stores/root.js
+++ src/stores/root.js
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import { loadState } from '@nextcloud/initial-state'
 import { useSessionStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
 import { editorApiAttachments, editorApiReaderFileId, editorApiUpdateReadonlyBarProps } from '../constants.js'
-import { loadState } from '@nextcloud/initial-state'
 
 const STORE_PREFIX = 'collectives/pinia/root/'
 
