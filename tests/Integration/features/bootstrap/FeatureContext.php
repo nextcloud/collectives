d26e382a05022397c8483e8bd23a35f68c320efa
Max <max@nextcloud.com>
2026-09-16 08:04:19 +0200

    chore(lint): php remove empty lines
    Signed-off-by: Max <max@nextcloud.com>

--- tests/Integration/features/bootstrap/FeatureContext.php
+++ tests/Integration/features/bootstrap/FeatureContext.php
@@ -190,7 +190,6 @@ class FeatureContext implements Context {
 		}
 	}
 
-
 	/**
 	 * @Then user :user sees collective :collective
 	 * @Then user :user sees collective :collective in :trash
@@ -2335,7 +2334,6 @@ class FeatureContext implements Context {
 			Assert::assertNotContains($name, $collectiveNames);
 		}
 	}
-
 
 	private function assertCollectiveKeyValue(string $name, string $key, string $value, ?bool $revert = false): void {
 		$jsonBody = $this->getJson();
