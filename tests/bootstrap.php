814cb93837ec8184b16464d17bec096682cba82c
Côme Chilliet <come.chilliet@nextcloud.com>
2026-09-23 12:00:17 +0200

    fix: Remove legacy method calls from tests/bootstrap.php
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+use OCP\App\IAppManager;
+use OCP\Server;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+define('PHPUNIT_RUN', 1);
 
 $nextcloud_source = getenv('NEXTCLOUD_SOURCE')
   ?: __DIR__ . '/../../..';
 require_once $nextcloud_source . '/tests/bootstrap.php';
 
-// Fix for "Autoload path not allowed: .../collectives/tests/testcase.php"
-OC_App::loadApp('collectives');
-OC_Hook::clear();
+Server::get(IAppManager::class)->loadApp('collectives');
